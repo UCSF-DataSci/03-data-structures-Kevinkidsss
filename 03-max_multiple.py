80c1291b7b6e567cbb586f478956f2f53c5d9a2a
Kevin Sun <159496424+Kevinkidsss@users.noreply.github.com>
2024-09-30 04:14:06 +0000

    Multiple

--- 03-max_multiple.py
+++ 03-max_multiple.py
@@ -42,7 +42,13 @@ def find_greatest_product(number_string, adjacent_digits=13):
     max_product = 0
     
     # Your code here
-
+    for i in range (len(number_string)-adjacent_digits + 1):
+        current_digits = number_string[i:i + adjacent_digits]
+        product = 1
+        for digit in current_digits:
+            product *= int(digit)
+        if product > max_product:
+            max_product = product
     return max_product
 
 if __name__ == "__main__":
